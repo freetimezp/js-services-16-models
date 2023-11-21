18668f9d1a589f2267bcaf64a5630d28d6255b46
freetimezp <kraftukrnet@ukr.net>
2023-11-21 22:31:32 +0200

    04. js-services-16-models. Banner section. Bg video. Change bg video.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,24 @@ function toggleMenu() {
 
     menu.classList.toggle('active');
     navMobile.classList.toggle('active');
-}
+};
+
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const videoContainer = document.querySelectorAll('.video-container');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    });
+
+    videoContainer.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    });
+};
+
